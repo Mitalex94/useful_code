901252d61c713239e3a9d0bde8e7593e18b1f116
Mitalex94 <62177712+Mitalex94@users.noreply.github.com>
2021-03-09 17:00:09 +0300

    Update generate_sentances.py

--- generate_sentances.py
+++ generate_sentances.py
@@ -6,6 +6,10 @@ import random
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
 
+def clear_last(posibilities, count):
+    for _ in range(count):
+        posibilities[posibilities.argmax()]=0
+
 def generate_sentances(our_sentance, amount, length):
     for j in range (amount):   
         sentance = our_sentance
@@ -13,17 +17,9 @@ def generate_sentances(our_sentance, amount, length):
             inputs = tokenizer(sentance, return_tensors="pt")
             outputs = model(**inputs, labels=inputs["input_ids"])
             logits = outputs.logits.detach().numpy()
-            mark = random.randint(1,3)
-            if mark == 1: 
-                sentance += tokenizer.decode(int(softmax(logits[0, -1]).argmax()))
-            elif mark == 2:
-                x = softmax(logits[0, -1])
-                x[x.argmax()]=0
-                sentance += tokenizer.decode(int(x.argmax()))
-            elif mark == 3:
-                x = softmax(logits[0, -1])
-                x[x.argmax()]=0
-                sentance += tokenizer.decode(int(x.argmax()))
+            x = softmax(logits[0, -1])
+            clear_last(x, random.randint(1,3))
+            sentance += tokenizer.decode(int(x.argmax()))
         print(sentance)
         
 sentence = input()
